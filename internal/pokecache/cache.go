7b58af91eacce4e88d1dc24fdd2c092b2976a9d7
brendenwelch <aloofd@gmail.com>
2025-06-18 17:22:01 -0700

    Created pokecache internal package.

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,60 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entry map[string]cacheEntry
+	mu    sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	entry, exists := c.entry[key]
+	c.mu.Unlock()
+	if !exists {
+		return nil, false
+	}
+	return entry.val, true
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	c.entry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.mu.Unlock()
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	nextReap := time.Now().Add(interval)
+	for {
+		if until := time.Until(nextReap); until > 0 {
+			time.Sleep(until)
+		}
+
+		c.mu.Lock()
+		for key, entry := range c.entry {
+			if time.Since(entry.createdAt) >= interval {
+				delete(c.entry, key)
+			}
+		}
+		c.mu.Unlock()
+
+		nextReap = time.Now().Add(interval)
+	}
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{}
+	go c.reapLoop(interval)
+	return c
+}
